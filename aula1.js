e33c36fd7854e3f97d63242834f76dbbb6f5b7e0
lu4nS <luansillima@gmail.com>
2024-06-27 11:01:13 -0300

    version 2

--- aula1.js
+++ aula1.js
@@ -1,18 +1,25 @@
-/* const nome = prompt("Qual é o seu nome?");
-const mensagem = `Olá, ${nome}! Seja bem-vindo(a).`;
-console.log(mensagem); */
 
-/* const numero1 = prompt("Digite o primeiro número:");
-const numero2 = prompt("Digite o segundo número:");
-const soma = parseInt(numero1) + parseInt(numero2);
-console.log(`A soma dos números ${numero1} e ${numero2} é ${soma}.`); */
+var aula1 = {
+    function1: function () {
+        const nome = prompt("Qual é o seu nome?");
+        const mensagem = `Olá, ${nome}! Seja bem-vindo(a).`
+        alert(mensagem);
+    },
+    function2: function () {
+        const numero1 = prompt("Digite o primeiro número:");
+        const numero2 = prompt("Digite o segundo número:");
+        const soma = parseInt(numero1) + parseInt(numero2);
+        alert(`A soma dos números ${numero1} e ${numero2} é ${soma}.`)
+    },
+    function3: function () {
+        const idade = prompt("Digite sua idade:");
+        const anoNascimento = 2024 - parseInt(idade);
+        alert(`Você nasceu em ${anoNascimento}.`)
+    },
+    function4: function () {
+        const raio = prompt("Digite o raio do círculo:");
+        const area = Math.PI * Math.pow(parseFloat(raio), 2);
+        alert(`A área do círculo é de aproximadamente ${area.toFixed(2)} unidades quadradas.`)
+    }
+}
 
-
-/* const idade = prompt("Digite sua idade:");
-const anoNascimento = 2024 - parseInt(idade);
-console.log(`Você nasceu em ${anoNascimento}.`); */
-
-
-const raio = prompt("Digite o raio do círculo:");
-const area = Math.PI * Math.pow(parseFloat(raio), 2);
-console.log(`A área do círculo é de aproximadamente ${area.toFixed(2)} unidades quadradas.`);
--- aula2.js
+++ aula2.js
@@ -1,162 +1,151 @@
 
-function function1() {
-    //programa1
-    const userInput = prompt("Enter a number:");
+var aula2 = {
+    function1: function () {
+        //programa1
+        const userInput = prompt("Enter a number:");
 
-    if (userInput !== null) {
-        const num = parseInt(userInput, 10);
+        if (userInput !== null) {
+            const num = parseInt(userInput, 10);
 
-        if (isNaN(num)) {
-            alert("Invalid input. Please enter a valid number.");
-        } else {
-            if (num > 0 && num % 2 === 0) {
-                alert(`The number ${num} is even and positive.`);
+            if (isNaN(num)) {
+                alert("Invalid input. Please enter a valid number.");
             } else {
-                alert(`The number ${num} is neither positive or even.`);
+                if (num > 0 && num % 2 === 0) {
+                    alert(`The number ${num} is even and positive.`);
+                } else {
+                    alert(`The number ${num} is neither positive or even.`);
+                }
             }
         }
-    }
-}
+    },
 
+    function2: function () {
+        //programa 2
+        const weight = prompt("Enter your weight in kilograms:");
 
-function function2() {
+        const height = prompt("Enter your height in meters:");
 
+        const bmi = calculateBMI(weight, height);
 
-    //programa 2
-    const weight = prompt("Enter your weight in kilograms:");
+        displayBMI(bmi);
 
-
-    const height = prompt("Enter your height in meters:");
-
-    const bmi = calculateBMI(weight, height);
-
-    displayBMI(bmi);
-
-    function calculateBMI(weight, height) {
-        return weight / (height * height);
-    }
-
-    function displayBMI(bmi) {
-        let message;
-
-        if (bmi < 16) {
-            message = "magreza grave";
-        } else if (bmi >= 16 && bmi <= 16.9) {
-            message = "magreza moderada";
-        } else if (bmi >= 17 && bmi <= 18.5) {
-            message = "magreza leve";
-        } else if (bmi >= 18.6 && bmi <= 24.9) {
-            message = "peso ideal"
-        } else if (bmi >= 25 && bmi <= 29.9) {
-            message = "sobrepeso"
-        } else if (bmi >= 30 && bmi <= 34.9) {
-            message = "obesidade grau 1"
-        } else if (bmi >= 35 && bmi < 39.9) {
-            message = "obesidade grau 2 severa"
-        } else if (bmi >= 40) {
-            message = "obesidade morbida"
+        function calculateBMI(weight, height) {
+            return weight / (height * height);
         }
 
-        alert(`Seu IMC eh ${bmi.toFixed(2)}. ${message}`);
-    }
-}
+        function displayBMI(bmi) {
+            let message;
 
+            if (bmi < 16) {
+                message = "magreza grave";
+            } else if (bmi >= 16 && bmi <= 16.9) {
+                message = "magreza moderada";
+            } else if (bmi >= 17 && bmi <= 18.5) {
+                message = "magreza leve";
+            } else if (bmi >= 18.6 && bmi <= 24.9) {
+                message = "peso ideal"
+            } else if (bmi >= 25 && bmi <= 29.9) {
+                message = "sobrepeso"
+            } else if (bmi >= 30 && bmi <= 34.9) {
+                message = "obesidade grau 1"
+            } else if (bmi >= 35 && bmi < 39.9) {
+                message = "obesidade grau 2 severa"
+            } else if (bmi >= 40) {
+                message = "obesidade morbida"
+            }
 
+            alert(`Seu IMC eh ${bmi.toFixed(2)}. ${message}`);
+        }
+    },
+    function3: function () {
+        //programa 3
+        const age = prompt("Enter your age:");
 
-function function3() {
+        const price = prompt("Enter the price of the product:");
 
-    //programa 3
-    const age = prompt("Enter your age:");
+        const finalPrice = calculateFinalPrice(age, price);
 
-    const price = prompt("Enter the price of the product:");
+        alert(`The final price is ${finalPrice}`);
 
-    const finalPrice = calculateFinalPrice(age, price);
+        function calculateFinalPrice(age, price) {
+            if (age < 18) {
+                return price * 0.9;
+            } else {
+                return price;
+            }
+        }
+    },
+    function4: function () {
+        //programa 4
+        const letter = prompt("Enter a letter:");
 
-    alert(`The final price is ${finalPrice}`);
+        const lowerCaseLetter = letter.toLowerCase();
 
-    function calculateFinalPrice(age, price) {
-        if (age < 18) {
-            return price * 0.9;
+        if (lowerCaseLetter === 'a' || lowerCaseLetter === 'e' || lowerCaseLetter === 'i' || lowerCaseLetter === 'o' || lowerCaseLetter === 'u') {
+            alert(`The letter ${letter} is a vowel.`);
         } else {
-            return price;
+            if (lowerCaseLetter.match(/[a-z]/)) {
+                alert(`The letter ${letter} is a consonant.`);
+            } else {
+                alert(`Invalid input. Please enter a letter.`);
+            }
         }
-    }
-}
+    },
+    function5: function () {
+        //programa 5
 
+        const dayNumber = parseInt(prompt("Enter a number between 1 and 7:"));
 
-function function4() {
-    //programa 4
-    const letter = prompt("Enter a letter:");
+        if (dayNumber >= 1 && dayNumber <= 7) {
 
-    const lowerCaseLetter = letter.toLowerCase();
-
-    if (lowerCaseLetter === 'a' || lowerCaseLetter === 'e' || lowerCaseLetter === 'i' || lowerCaseLetter === 'o' || lowerCaseLetter === 'u') {
-        alert(`The letter ${letter} is a vowel.`);
-    } else {
-        if (lowerCaseLetter.match(/[a-z]/)) {
-            alert(`The letter ${letter} is a consonant.`);
+            switch (dayNumber) {
+                case 1:
+                    alert("Monday");
+                    break;
+                case 2:
+                    alert("Tuesday");
+                    break;
+                case 3:
+                    alert("Wednesday");
+                    break;
+                case 4:
+                    alert("Thursday");
+                    break;
+                case 5:
+                    alert("Friday");
+                    break;
+                case 6:
+                    alert("Saturday");
+                    break;
+                case 7:
+                    alert("Sunday");
+                    break;
+            }
         } else {
-            alert(`Invalid input. Please enter a letter.`);
+            alert("Invalid input. Please enter a number between 1 and 7.");
         }
-    }
-}
+    },
 
-function function5() {
-    //programa 5
-
-    const dayNumber = parseInt(prompt("Enter a number between 1 and 7:"));
-
-    if (dayNumber >= 1 && dayNumber <= 7) {
-
-        switch (dayNumber) {
-            case 1:
-                alert("Monday");
-                break;
-            case 2:
-                alert("Tuesday");
-                break;
-            case 3:
-                alert("Wednesday");
-                break;
-            case 4:
-                alert("Thursday");
-                break;
-            case 5:
-                alert("Friday");
-                break;
-            case 6:
-                alert("Saturday");
-                break;
-            case 7:
-                alert("Sunday");
-                break;
-        }
-    } else {
-        alert("Invalid input. Please enter a number between 1 and 7.");
-    }
-}
-
-function function6() {
-    //programa 6
-
-    function isPrime(n) {
-        if (n <= 1) {
-            return false; 
-        }
-        for (let i = 2; i * i <= n; i++) {
-            if (n % i === 0) {
+    function6: function () {
+        function isPrime(n) {
+            if (n <= 1) {
                 return false;
             }
+            for (let i = 2; i * i <= n; i++) {
+                if (n % i === 0) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true; 
-    }
 
-    const num = parseInt(prompt("Enter a number:"))
-    
-    if (isPrime(num)) {
-        alert(`${num} is a prime number.`);
-    } else {
-        alert(`${num} is not a prime number.`);
-    }
+        const num = parseInt(prompt("Enter a number:"))
 
+        if (isPrime(num)) {
+            alert(`${num} is a prime number.`);
+        } else {
+            alert(`${num} is not a prime number.`);
+        }
+    }
 }
+
